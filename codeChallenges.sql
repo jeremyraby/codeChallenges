1f02869a27003c7296010f406d41772308c31ad1
jeremyraby <jeremy.raby10@gmail.com>
2023-01-17 19:34:04 -0700

    Add query for students and teachers tables.

--- codeChallenges.sql
+++ codeChallenges.sql
@@ -351,3 +351,15 @@ VALUES (
     'Julia', 'Marrero',
     'Mary', 'Foulds'
 );
+
+-- Write a query to output all of the students with their teachers' last names in one table, sorted by students ID.
+SELECT 
+    students.id,
+    students.firstname,
+    students.lastname,
+    teachers.lastname AS "teacher"
+FROM students, teachers
+WHERE 
+    students.teacherid =
+    teachers.id
+ORDER BY students.id;
